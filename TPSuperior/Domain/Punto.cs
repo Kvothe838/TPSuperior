d246ed0d21b16d99f2815e89f14c48f7cffc80ac
Facundo Yuffrida <fyuffrida@baufest.com>
2019-10-11 15:43:16 -0300

    Creación de clase Punto y evaluación de un punto en Lagrange.

---
+++ TPSuperior/Domain/Punto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TPSuperior.Domain
+{
+    public class Punto
+    {
+        public double x { get; set; }
+
+        public double y { get; set; }
+
+        public List<Punto> otrosPuntos { get; set; }
+        
+        public double EvaluarPolinomio_Lagrange(double x)
+        {
+            double numerador = 1, denominador = 1;
+
+            foreach (Punto punto in otrosPuntos)
+            {
+                numerador *= (x - punto.x);
+                denominador *= (this.x - punto.x);
+            }
+
+            return numerador / denominador;
+        }
+    }
+}
--- TPSuperior/Program.cs
+++ TPSuperior/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace TPSuperior
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TPSuperior.Domain;
+
+namespace TPSuperior
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Pedir datos de puntos a interpolar
+            List<Punto> puntos = new List<Punto>();
 
+            Console.WriteLine("1. Ingresar nuevo punto");
+            Console.WriteLine("2. Evaluar polinomio interpolado en un punto");
+            string opcionSeleccionada = Console.ReadLine();
+
+            while (opcionSeleccionada == "1")
+            {
+                Punto punto = new Punto();
+
+                Console.Write("Introduzca x: ");
+                double tryX;
+
+                if (!double.TryParse(Console.ReadLine(), out tryX))
+                {
+                    //input incorrecto
+                }
+
+                punto.x = tryX;
+
+                Console.Write("\nIntroduzca y: ");
+                double tryY;
+
+                if (!double.TryParse(Console.ReadLine(), out tryY))
+                {
+                    //input incorrecto
+                }
+
+                punto.y = tryY;
+
+                puntos.Add(punto);
+
+                Console.WriteLine("\n1. Ingresar nuevo punto");
+                Console.WriteLine("2. Evaluar polinomio interpolado en un punto");
+                opcionSeleccionada = Console.ReadLine();
+            }
+
+            if (opcionSeleccionada == "2")
+            {
+                Console.Write("\nIngresar valor a evaluar: ");
+                double valorEvaluar;
+
+                if (!double.TryParse(Console.ReadLine(), out valorEvaluar))
+                {
+                    //input incorrecto
+                }
+
+                double resultado = Lagrange(puntos, valorEvaluar);
+                Console.WriteLine("Resultado: " + resultado);
+
+            }
+
+            //Console.WriteLine(puntos.Select(punto => "x: " + punto.x + ", y: " + punto.y).Aggregate((unMensaje, otroMensaje) => unMensaje + "\n" + otroMensaje));
+            Console.ReadKey();
+        }
+
+        static double Lagrange(List<Punto> puntos, double x)
+        {
+            for (int i = 0; i < puntos.Count; i++)
+            {
+                List<Punto> temp = puntos.Select(punto => (Punto)punto).ToList();
+                temp.RemoveAt(i);
+                puntos[i].otrosPuntos = temp;
+            }
+
+            return puntos.Select(punto => punto.EvaluarPolinomio_Lagrange(x) * punto.y)
+                .Aggregate((unValor, otroValor) => unValor + otroValor);
         }
     }
 }
